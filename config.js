375c82a4b7d7de770f831dd2a484c55496e5e5e8
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-06-05 00:16:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.web = 'https://github.com/romlisuhanda' //ubah jadi website lu, bisa link
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/39d144c866ddd0db1ac1e.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/17edeb9ade79777e1c45d.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ZxyuuBOTZ-MD'
